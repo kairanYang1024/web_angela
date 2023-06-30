e9141587126f00c7e8d2ad029242ceceb8d5b76e
kairanYang1024 <kyang2333@gmail.com>
2023-06-30 07:20:02 +0000

    27

--- exercises/27+EJS/app.js
+++ exercises/27+EJS/app.js
@@ -2,12 +2,42 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello");
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public")); //static resource folder: public/
+
+const activities = ["Work hard!","Sleep late!", "Anxious for your miserable future!"]
+const tasks = []
+
+app.get("/", (req, res)=>{
+    const day = date.getDate();
+    res.render("list", {listTitle:day, activities:activities}); //filename inside views/ the second entry specifies the variable to parse into the template
 });
+
+app.get("/work", (req, res)=>{
+    res.render("list", {listTitle:"Work List", activities:tasks}); 
+});
+
+app.get("/about", (req, res)=>{
+    res.render("about");
+});
+
+app.post("/", (req, res)=> {
+    const newstuff = req.body.garbage;
+    if(req.body.list === "Work List") {
+        tasks.push(newstuff);
+        res.redirect("/work");
+    } else {
+        activities.push(newstuff);
+        res.redirect("/");
+    }
+});
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
---
+++ exercises/27+EJS/date.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+module.exports = {getDate};
+
+function getDate() {
+    const today = new Date();
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    return today.toLocaleDateString("en-US", options);
+}
+
+
